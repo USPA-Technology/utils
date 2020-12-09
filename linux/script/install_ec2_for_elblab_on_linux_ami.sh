df817dfcce31ddfd0112a615e487dac9e45aeaea
phan2410 <phan2410@gmail.com>
2020-12-09 18:25:49 +0700

    add elblab setup script

---
+++ linux/script/install_ec2_for_elblab_on_linux_ami.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo yum -y install httpd php
+sudo chkconfig httpd on
+sudo systemctl start httpd.service
+cd /var/www/html
+sudo wget https://s3-us-west-2.amazonaws.com/us-west-2-aws-training/awsu-spl/spl-03/scripts/examplefiles-elb.zip
+sudo unzip examplefiles-elb.zip
